7eb63ae02699b34097d7b14fced143b6ccfdb536
Antritus <antt.graft@gmail.com>
2024-02-29 16:19:33 +0200

    Add constructor to PlaceholderList

--- src/main/java/bet/astral/messenger/placeholder/PlaceholderList.java
+++ src/main/java/bet/astral/messenger/placeholder/PlaceholderList.java
@@ -1,10 +1,19 @@
 package bet.astral.messenger.placeholder;
 
 import bet.astral.messenger.utils.PlaceholderUtils;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
 import java.util.LinkedList;
 
 public class PlaceholderList extends LinkedList<Placeholder> {
+	public PlaceholderList() {
+	}
+
+	public PlaceholderList(@NotNull Collection<? extends Placeholder> c) {
+		super(c);
+	}
+
 	public boolean add(String name, Object info) {
 		return add(PlaceholderUtils.createPlaceholder(null, name, info));
 	}
