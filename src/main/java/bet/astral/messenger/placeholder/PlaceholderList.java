35ac741f9cb4aae5d7893032e2ecc6a59bc2d35e
Antritus <antt.graft@gmail.com>
2024-03-13 15:43:52 +0200

    More ways to add placeholders

--- src/main/java/bet/astral/messenger/placeholder/PlaceholderList.java
+++ src/main/java/bet/astral/messenger/placeholder/PlaceholderList.java
@@ -3,8 +3,10 @@ package bet.astral.messenger.placeholder;
 import bet.astral.messenger.utils.PlaceholderUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 public class PlaceholderList extends LinkedList<Placeholder> {
 	public PlaceholderList() {
@@ -19,6 +21,18 @@ public class PlaceholderList extends LinkedList<Placeholder> {
 	}
 	public void add(int index, String name, Object info) {
 		add(index, PlaceholderUtils.createPlaceholder(null, name, index));
+	}
+
+	public void addAll(Placeholder... placeholders){
+		this.addAll(Arrays.asList(placeholders));
+	}
+	public void addAll(String prefix, Placeholderable... placeholderables){
+		this.addAll(prefix, Arrays.asList(placeholderables));
+	}
+	public void addAll(String prefix, List<Placeholderable> placeholderables){
+		for (Placeholderable placeholderable : placeholderables){
+			this.addAll(placeholderable.asPlaceholder(prefix));
+		}
 	}
 
 	public void add(String prefix, Placeholderable placeholderable){
