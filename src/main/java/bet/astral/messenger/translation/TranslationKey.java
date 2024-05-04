a46ece05a319a0bf0e857f400fd60a1ad4be7840
Antritus <antt.graft@gmail.com>
2024-05-04 19:29:46 +0300

    Fix translation package being incorrectly named and add of(key) to TranslationKey

--- src/main/java/bet/astral/messenger/translation/TranslationKey.java
+++ src/main/java/bet/astral/messenger/translation/TranslationKey.java
@@ -1,9 +1,12 @@
-package bet.astral.messenger.trasnlation;
+package bet.astral.messenger.translation;
 
 import net.kyori.adventure.translation.Translatable;
 import org.jetbrains.annotations.NotNull;
 
 public class TranslationKey implements Translatable {
+	public static TranslationKey of(String key){
+		return new TranslationKey(key);
+	}
 	private final String key;
 
 	public TranslationKey(@NotNull String key) {
