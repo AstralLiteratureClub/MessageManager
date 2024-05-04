13ec83c3bb73497eea8b8ed036e3d2799ba6ad72
Antritus <antt.graft@gmail.com>
2024-05-04 19:34:45 +0300

    Add cloud caption stuff to translation keys

--- src/main/java/bet/astral/messenger/translation/TranslationKey.java
+++ src/main/java/bet/astral/messenger/translation/TranslationKey.java
@@ -1,12 +1,21 @@
 package bet.astral.messenger.translation;
 
 import net.kyori.adventure.translation.Translatable;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.incendo.cloud.caption.Caption;
+import org.incendo.cloud.minecraft.extras.caption.TranslatableCaption;
 import org.jetbrains.annotations.NotNull;
 
-public class TranslationKey implements Translatable {
+public class TranslationKey implements Translatable, Caption {
 	public static TranslationKey of(String key){
 		return new TranslationKey(key);
 	}
+
+	@Override
+	public @NonNull String key() {
+		return key;
+	}
+
 	private final String key;
 
 	public TranslationKey(@NotNull String key) {
@@ -17,4 +26,9 @@ public class TranslationKey implements Translatable {
 	public @NotNull String translationKey() {
 		return key;
 	}
+
+	public @NotNull Caption asTranslatableCaption(){
+		return TranslatableCaption.translatableCaption(key);
+	}
 }
+
