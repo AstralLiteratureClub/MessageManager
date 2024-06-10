b011534507ce3d4b282c3b287c08775046b53fac
Antritus <antt.graft@gmail.com>
2024-06-10 09:49:59 +0300

    Set default messenger in paper messenger

--- paper/src/main/java/bet/astral/messenger/v2/paper/PaperMessenger.java
+++ paper/src/main/java/bet/astral/messenger/v2/paper/PaperMessenger.java
@@ -1,11 +1,14 @@
 package bet.astral.messenger.v2.paper;
 
 import bet.astral.messenger.v2.AbstractMessenger;
+import bet.astral.messenger.v2.DefaultScheduler;
 import bet.astral.messenger.v2.paper.receiver.ConsoleReceiver;
+import bet.astral.messenger.v2.paper.scheduler.ASyncScheduler;
 import bet.astral.messenger.v2.receiver.Receiver;
 import org.bukkit.plugin.Plugin;
 import org.slf4j.Logger;
 
+import java.lang.reflect.Field;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -16,6 +19,15 @@ public class PaperMessenger extends AbstractMessenger {
 	public static void init(Plugin plugin){
 		if (PLUGIN != null && PLUGIN.isEnabled()){
 			return;
+		}
+		Class<DefaultScheduler> defaultSchedulerClass = DefaultScheduler.class;
+		try {
+			Field field = defaultSchedulerClass.getDeclaredField("ASYNC_SCHEDULER");
+			field.setAccessible(true);
+			field.set(null, ASyncScheduler.SCHEDULER);
+			field.setAccessible(false);
+		} catch (NoSuchFieldException | IllegalAccessException e) {
+			throw new RuntimeException(e);
 		}
 		PLUGIN = plugin;
 		playerManager = new PlayerManager();
